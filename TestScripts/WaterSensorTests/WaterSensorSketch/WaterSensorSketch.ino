370fa62ed8ee638b993fa4c121c3bf3b68c7a7a9
ReeSuro <surowiezs@gmail.com>
2021-08-03 12:33:10 +1200

    Saving Progress
    No major changes

---
+++ TestScripts/WaterSensorTests/WaterSensorSketch/WaterSensorSketch.ino
@@ -0,0 +1,16 @@
+const int pin = D6;
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = digitalRead(pin);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1000);        // delay in between reads for stability
+}
